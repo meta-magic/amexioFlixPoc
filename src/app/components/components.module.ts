2a26c820d4858a2eb0e5919f23a583326ac1306d
DatsGawas <dattaram.gawas@metamagic.in>
2017-08-23 16:16:34 +0530

    tab added

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,19 @@
  * Created by pratik on 23/8/17.
  */
 import { NgModule } from '@angular/core';
-import {DeferredLoader} from "./defer/defer.page";
-import {CommonModule} from "@angular/common";
-import {MultiItemCarousel} from "./multi-item-carousel/multi.item.carousel.component";
+import {DeferredLoader} from './defer/defer.page';
+import {CommonModule} from '@angular/common';
+import {MultiItemCarousel} from './multi-item-carousel/multi.item.carousel.component';
+import {PaneActionComponent} from './panes/paneaction.component';
+import {PaneBodyComponent} from './panes/panebody.component';
+import {CardComponent} from './panes/card.component';
+import {TabPaneComponent} from './tabpane/tabpane.component';
+import {TabComponent} from './tabpane/tabpill.component';
 
 const COMPONENTS = [
   DeferredLoader,
-  MultiItemCarousel
+  MultiItemCarousel,  TabPaneComponent, TabComponent,
+  CardComponent, PaneBodyComponent, PaneActionComponent
 ];
 
 @NgModule({
