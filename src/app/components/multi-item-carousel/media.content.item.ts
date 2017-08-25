20247b789d05c7b32979140cbe0ff53f93ffdbf8
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2017-08-26 00:12:43 +0530

    Sample For Media Content Description

--- src/app/components/multi-item-carousel/media.content.item.ts
+++ src/app/components/multi-item-carousel/media.content.item.ts
@@ -6,8 +6,14 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
  selector: 'amexio-media-content',
  template: `   
-   <div class="carosel-item" (mouseenter)="onHoverEnter()" (mouseleave)="onHoverLeave()">
-    <img src="{{imgPath}}" />
+   <div class="carosel-item img__wrap" (mouseenter)="onHoverEnter()" (mouseleave)="onHoverLeave()">
+    <img src="{{imgPath}}" class="img__img" />
+     <div class="img__description_layer">
+       <h6 class="img__description" style="color: white">
+         <p style="padding-left: 30%"><i class="fa fa-play fa-5x" aria-hidden="true"></i></p> 
+         <p>This image looks super neat.</p>
+       </h6>
+     </div>
    </div>
      
  `,
@@ -19,9 +25,13 @@ export class MediaContentItem implements OnInit {
   @Input() id : string;
   showText : boolean;
 
+  elementId : any;
+
   @Input() imgPath : any;
 
-  constructor() { }
+  constructor() {
+    this.elementId = 'media-content-item-' + Math.floor(Math.random()*90000) + 10000;
+  }
 
   ngOnInit() {
     console.log(this.imgPath);
