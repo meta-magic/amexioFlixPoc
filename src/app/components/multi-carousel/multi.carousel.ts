ac51a9d3df17301f082966db77304c85b69e7983
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2017-08-30 18:27:31 +0530

    Click event not firing jQuery Carousel Issue

--- src/app/components/multi-carousel/multi.carousel.ts
+++ src/app/components/multi-carousel/multi.carousel.ts
@@ -2,6 +2,7 @@
  * Created by pratik on 29/8/17.
  */
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 declare var $;
 @Component({
   selector: 'amexio-ee-multi-carousel',
@@ -58,8 +59,30 @@ export class AmexioMultiMediaCarouselComponent implements OnInit {
 
   text : string;
 
+  hasDetail : boolean = true;
+  ratingData : any[] = [1,2,3,4,5,6,7,8,9,10];
+  public max = 5;
+  public rate = 4;
 
-  constructor() {
+  videoUrl : string;
+
+  currentDetailsImagePath : string;
+
+  currentDetailsTitle : string;
+
+  currentDetailsDesc : string;
+
+  currentContent : string;
+
+  ageLimit: any;
+
+  releaseYear: any;
+
+  seasonNo: number;
+
+  matchPercentage: any;
+
+  constructor(private router : Router) {
 
   }
 
@@ -89,14 +112,6 @@ export class AmexioMultiMediaCarouselComponent implements OnInit {
       }
     });
 
-/*    $(document).on('mouseenter','.carousel-item', ()=>{
-      this.text = 'hover-item';
-    });
-
-    $(document).on('mouseleave','.carousel-item', ()=>{
-      this.text = 'gone';
-    });*/
-
   }
 
   onPrevious(){
@@ -112,6 +127,42 @@ export class AmexioMultiMediaCarouselComponent implements OnInit {
   }
   onHoverLeave(){
     this.hoverClass = '';
+  }
+
+
+  openDetailsSection(item : any) {
+    debugger;
+    this.videoUrl = item.video;
+    this.currentDetailsImagePath = item.details_img;
+    this.currentDetailsTitle = item.title;
+    this.currentDetailsDesc = item.desc;
+    this.currentContent = item.content;
+    this.seasonNo = item.seasonNo;
+    this.matchPercentage = item.matchPercentage;
+    this.ageLimit = item.ageLimit;
+    this.releaseYear = item.releaseYear;
+
+    $('#'+this.elementId+'carousel-detail-content').collapse('show');
+    setTimeout(()=>{
+      $('html, body').animate({
+        scrollTop: $('#'+this.elementId+'carousel-detail-content').offset().top
+      }, 500);
+    },500)
+  }
+
+  closeDetailPage(){
+    $('#'+this.elementId+'carousel-detail-content').collapse('hide');
+    $('html, body').animate({
+      scrollTop: $('#'+this.elementId).offset().top
+    }, 500);
+  }
+
+  loadVideo(item : any){
+    this.router.navigate(['/player',item.video])
+  }
+
+  playVideo(video : any){
+    this.router.navigate(['player',video]);
   }
 
 }
