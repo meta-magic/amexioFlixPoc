4a36b5066dbd7a3fb0ed354f8876c29d4513c838
ketangote <ketan.gote@metamagic.in>
2017-09-04 21:47:28 +0530

    Add more data to drama section

--- src/app/pages/landing-page/landing.page.component.ts
+++ src/app/pages/landing-page/landing.page.component.ts
@@ -275,7 +275,7 @@ export class LandingPageComponent implements OnInit {
        "title" : "13 Reasons Why",
        "img" : "assets/images/tvshows/shows/13_reasons_why_img.jpg",
        "details_img": "assets/images/tvshows/shows/13_reasons_details.jpg",
-       "desc" : "Set in a world of greed, betrayal, sexual intrigue and rivalry, “Marco Polo” is based on the famed explorer’s adventures in Kublai Khan’s court.",
+       "desc" : "After a teenage girl's perplexing suicide, a classmate receives a series of tapes that unravel the mystery of her tragic choice.",
        "video" : "hB-ltNasHVw",
        "matchPercentage" : "90",
        "releaseYear" : "2017",
@@ -284,11 +284,11 @@ export class LandingPageComponent implements OnInit {
        "content" : [
          {
            'key': 'Creator',
-           'value': 'John Fusco'
+           'value': 'Adam Wingard'
          },
          {
            'key': 'Cast',
-           'value': 'Chin Han, Joan Chen, Zhu Zhu, Tom Wu'
+           'value': 'Nat Wolf, Jason Lilles'
          },
          {
            'key': 'Genres',
@@ -308,24 +308,24 @@ export class LandingPageComponent implements OnInit {
        "title" : "Death Note",
        "img" : "assets/images/tvshows/shows/death_note.jpg",
        "details_img": "assets/images/tvshows/shows/death_notes_details.jpg",
-       "desc" : "The true story of Colombia's infamously violent and powerful drug cartels fuels this gritty gangster drama series.",
-       "video" : "U7elNhHwgBU",
-       "matchPercentage" : "95",
+       "desc" : "Light Turner finds a supernatural notebook and uses it to mete out death, attracting the attention of a detective, a demon and a girl in his class.",
+       "video" : "hB-ltNasHVw",
+       "matchPercentage" : "90",
        "releaseYear" : "2017",
        "ageLimit" : "16",
-       "seasonNo" : "3",
+       "seasonNo" : "1",
        "content" : [
          {
-           'key': 'Director',
-           'value': 'Clint Eastwood'
+           'key': 'Creator',
+           'value': 'Adam Wingard'
          },
          {
-           'key': 'Starring',
-           'value': '	Tom Hanks, Aaron Eckhart, Laura Linney'
+           'key': 'Cast',
+           'value': 'Nat Wolf, Jason Lilles'
          },
          {
            'key': 'Genres',
-           'value': 'Drama: Biography'
+           'value': 'TV Programmes'
          },
          {
            'key': 'Subtitles',
@@ -341,7 +341,7 @@ export class LandingPageComponent implements OnInit {
        "title" : "Little Evil",
        "img" : "assets/images/tvshows/shows/little_evil_img.jpg",
        "details_img": "assets/images/tvshows/shows/little_evil_details.jpg",
-       "desc" : "A highly decorated ex-Marine sniper returns to action to foil an assassination plot targeting the president, but soon finds himself framed for murder. ",
+       "desc" : "In this horror-comedy, a recently married man who wants to bond with his stepson begins to fear that the boy is a demon.",
        "video" : "X2x6IR3FhbQ",
        "matchPercentage" : "94",
        "releaseYear" : "2015",
@@ -370,7 +370,7 @@ export class LandingPageComponent implements OnInit {
        "title" : "Orange Black",
        "img" : "assets/images/tvshows/shows/orange_black.jpg",
        "details_img": "assets/images/tvshows/shows/the_orange_details.jpg",
-       "desc" : "A ruthless politician will stop at nothing to conquer Washington, D.C., in this Emmy and Golden Globe-winning political drama.",
+       "desc" : "A privileged New Yorker ends up in a women's prison when a past crime catches up with her in this Emmy-winning series from the creator of Weeds.",
        "video" : "ULwUzF1q5w4",
        "matchPercentage" : "90",
        "releaseYear" : "2017",
@@ -403,7 +403,7 @@ export class LandingPageComponent implements OnInit {
        "title" : "Aditi Mittal",
        "img" : "assets/images/tvshows/shows/aditi_mittal.jpg",
        "details_img": "assets/images/tvshows/shows/aditi_mittal_details.jpg",
-       "desc" : "While the conspirators make a surprise move, the FBI scrambles to arrest the ringleader. Kirkman asks Aaron to smoke out the White House traitor.",
+       "desc" : "Trailblazing comic Aditi Mittal mixes topical stand-up with frank talk about being single, wearing thongs and the awkwardness of Indian movie ratings.",
        "video" : "D_6J9BqgonU",
        "matchPercentage" : "94",
        "releaseYear" : "2015",
@@ -436,7 +436,7 @@ export class LandingPageComponent implements OnInit {
        "title" : "Fauda",
        "img" : "assets/images/tvshows/shows/fauda.jpg",
        "details_img": "assets/images/tvshows/shows/fauda_details.jpg",
-       "desc" : "A financial adviser drags his family from Chicago to the Missouri Ozarks, where he must launder $500 million in five years to appease a drug boss.",
+       "desc" : "A top Israeli agent comes out of retirement to hunt for a Palestinian militant he thought he'd killed, setting a chaotic chain of events into motion.",
        "video" : "5hAXVqrljbs",
        "matchPercentage" : "95",
        "releaseYear" : "2017",
@@ -469,7 +469,7 @@ export class LandingPageComponent implements OnInit {
        "title" : "The Mist",
        "img" : "assets/images/tvshows/shows/the_mist.jpg",
        "details_img": "assets/images/tvshows/shows/the_mist_details.jpg",
-       "desc" : "This drama follows the political rivalries and romance of Queen Elizabeth II's reign and the events that shaped the second half of the 20th century.",
+       "desc" : "Trapped by a mysterious fog, residents of a Maine village discover that it hides nightmarish creatures. A new series based on Stephen King's novella.",
        "video" : "JWtnJjn6ng0",
        "matchPercentage" : "94",
        "releaseYear" : "2015",
@@ -502,7 +502,7 @@ export class LandingPageComponent implements OnInit {
        "title" : "The Siege",
        "img" : "assets/images/tvshows/shows/the_siege.jpg",
        "details_img": "assets/images/tvshows/shows/the_seige_details.jpg",
-       "desc" : "With humanity teetering on the brink of extinction, a group of astronauts travels through a wormhole in search of another inhabitable planet.",
+       "desc" : "Besieged by overwhelming enemy forces, Irish soldiers on a U.N. peacekeeping mission in Africa valiantly defend their outpost in this true story.",
        "video" : "nIgeVoVMv90",
        "matchPercentage" : "90",
        "releaseYear" : "2017",
@@ -700,9 +700,9 @@ export class LandingPageComponent implements OnInit {
        ]
      },
      {
-       "title" : "A Pretty Little Liar",
-       "img" : "assets/images/newrelease/a.jpg",
-       "details_img": "assets/images/newrelease/a_details.jpg",
+       "title" : "A Big Short",
+       "img" : "assets/images/drama/the_big_short.jpg",
+       "details_img": "assets/images/drama/the_big_short_details.jpg",
        "desc" : "To avenge his father's death, a young man leads an army into battle against a cruel tyrant in this Hindi conclusion to the historic saga.",
        "video" : "Xx---l5CoMQ",
        "matchPercentage" : "95",
@@ -733,9 +733,9 @@ export class LandingPageComponent implements OnInit {
        ]
      },
      {
-       "titile" : "Banking On Bitcoin",
-       "img" : "assets/images/newrelease/bankingonbitcoin.jpg",
-       "details_img": "assets/images/newrelease/bitcoin_details.jpg",
+       "titile" : "American Sniper",
+       "img" : "assets/images/drama/american_sniper.jpg",
+       "details_img": "assets/images/drama/american_sniper_details.jpg",
        "desc" : "A once-promising wrestler pursues the gold medal he never won by training two of his daughters to compete at the Commonwealth Games.",
        "video" : "CTbyaj8Y-Co",
        "matchPercentage" : "94",
@@ -762,9 +762,9 @@ export class LandingPageComponent implements OnInit {
        ]
      },
      {
-       "title" : "Naked",
-       "img" : "assets/images/newrelease/naked.jpg",
-       "details_img": "assets/images/newrelease/naked_details.jpg",
+       "title" : "Dilwale",
+       "img" : "assets/images/drama/dilwale.jpg",
+       "details_img": "assets/images/drama/diwale_details.jpg",
        "desc" : "With humanity teetering on the brink of extinction, a group of astronauts travels through a wormhole in search of another inhabitable planet.",
        "video" : "nIgeVoVMv90",
        "matchPercentage" : "90",
@@ -795,9 +795,9 @@ export class LandingPageComponent implements OnInit {
        ]
      },
      {
-       "title" : "Rustom",
-       "img" : "assets/images/newrelease/rutstom.jpg",
-       "details_img": "assets/images/newrelease/rustom_details.jpg",
+       "title" : "Queen",
+       "img" : "assets/images/drama/queen.jpg",
+       "details_img": "assets/images/drama/queen_details.jpg",
        "desc" : "After street smart kid Raees rises to become Gujarat's bootlegging king in the 1980s, he tries to balance his life of crime with helping his people.",
        "video" : "L83qMnbJ198",
        "matchPercentage" : "94",
@@ -828,9 +828,9 @@ export class LandingPageComponent implements OnInit {
        ]
      },
      {
-       "title" : "Sand Of Castle",
-       "img" : "assets/images/newrelease/sandcatle.jpg",
-       "details_img": "assets/images/newrelease/sand_cattle_details.jpg",
+       "title" : "Focus",
+       "img" : "assets/images/drama/focus.jpg",
+       "details_img": "assets/images/drama/focus_details.jpg",
        "desc" : "When a proud general is tasked with winning an unpopular war, he takes the challenge head-on, not knowing that hubris may be his own worst enemy.",
        "video" : "8hq28cZ938E",
        "matchPercentage" : "90",
